ac27bb271128f7d51c403004e5481b181c018284
Vijay Sikha <vksikha@gmail.com>
2018-11-20 23:58:37 -0600

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello Tyson Foods"}
+		res := &response{Message: "Hello Vijay"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
